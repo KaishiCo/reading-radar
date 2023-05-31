61710b173eb0e09352af640beb92bd83a37f0698
Marc <ninjaz.gg@outlook.com>
2023-05-31 17:41:40 -0400

    add logging (w/ serilog)

--- src/ReadingRadar.Api/DependencyInjection.cs
+++ src/ReadingRadar.Api/DependencyInjection.cs
@@ -1,13 +1,19 @@
 using ReadingRadar.Api.Health;
+using Serilog;
 
 namespace ReadingRadar.Api;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    public static IServiceCollection AddPresentation(
+        this IServiceCollection services,
+        IHostBuilder host)
     {
         services.AddHealthChecks()
             .AddCheck<DatabaseHealthCheck>("Database");
+
+        host.UseSerilog((context, logger) =>
+            logger.ReadFrom.Configuration(context.Configuration));
         return services;
     }
 
